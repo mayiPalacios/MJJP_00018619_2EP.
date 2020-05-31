9f466eed980ce45685f1e74b61d5b92edf24e4fc
mayiPalacios <00018619@uca.edu.sv>
2020-05-31 17:35:45 -0600

    Add:Recuperacion

---
+++ Recuperacion/Preparcial/Vista/FrmLogin.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using Preparcial.Controlador;
+using Preparcial.Modelo;
+using Preparcial.Vista;
+
+namespace Preparcial
+{
+    public partial class FrmLogin : Form
+    {
+        public FrmLogin()
+        {
+            InitializeComponent();
+        }
+
+        // Evento Load del FrmLogin, para cargar la imagen del logo UCA desde la carpeta root
+        private void FrmLogin_Load(object sender, EventArgs e)
+        {
+            pictureBox1.BackgroundImage = Image.FromFile("../../Recursos/UCA.png");
+            pictureBox1.BackgroundImageLayout = ImageLayout.Stretch;
+
+            PoblarControlers();
+        }
+        
+        // Metodo para poblar el comboBox con todos los usuarios, asignando el value member y display member desde
+        // las PROPIEDADES de la clase Usuario, deben ser publicas y llamarse igual para que funcione, ademas
+        // primero se asigna el ValueMember, luego DisplayMember, y por ultimo el DataSource, sino tira excepcion
+        private void PoblarControlers()
+        {
+            cmbUser.DataSource = null;
+            //correcion:se cambio el value member por nameof y se mando los parametros de la contrasena
+            cmbUser.ValueMember = nameof(Usuario.Contrasena);
+            cmbUser.DisplayMember = "NombreUsuario";
+            cmbUser.DataSource = ControladorUsuario.GetUsuarios();
+        }
+
+        // EventHandler para detectar el click para inciar sesion
+        private void BttnLogin_Click(object sender, EventArgs e)
+        {
+            // Si la contrasena del textBox es igual al SelectedValue del comboBox, entonces iniciar sesion
+            if (textBox1.Text.Equals(cmbUser.SelectedValue.ToString()))
+            {
+                // Crear ventana principal enviandole el item seleccionado
+                // Recordar que SelectedItem devuelve un objeto, con polimorfismo se convierte a Usuario
+                //correcion:  primero creemos el objeto de Usuario
+                Usuario us = (Usuario) cmbUser.SelectedItem;
+                //correcion: ahora instanciamos del formMain y le mandamos como parametro el objeto de Usuario
+                FrmMain frmMain = new FrmMain(us);
+                // correcion: Esconder frmLogin y mostrar frmMain
+                Hide();
+                frmMain.Show();
+            }
+            else
+                MessageBox.Show("Contrasena incorrecta");
+        }
+
+        // EventHandler para invocar al form de actualizar contrasena
+        private void BttnUpdatePassword_Click(object sender, EventArgs e)
+        {
+            
+            FrmPassword frmPassword = new FrmPassword();
+            //correcion: cambiamos de show a showdialog
+            frmPassword.ShowDialog();
+            //correcion: y actualizamos los controles
+            PoblarControlers();
+        }
+    }
+}
---
+++ Recuperacion/Preparcial/Vista/FrmPassword.cs
@@ -0,0 +1,54 @@
+﻿using Preparcial.Controlador;
+using Preparcial.Modelo;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Preparcial.Vista
+{
+    public partial class FrmPassword : Form
+    {
+        public FrmPassword()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmPassword_Load(object sender, EventArgs e)
+        {
+            pictureBox1.BackgroundImage = Image.FromFile("../../Recursos/UCA.png");
+pictureBox1.BackgroundImageLayout = ImageLayout.Stretch;
+
+ActualizarControlers();
+}
+
+private void ActualizarControlers()
+{
+comboBox1.ValueMember = "Contrasena";
+comboBox1.DataSource = ControladorUsuario.GetUsuarios();
+comboBox1.DisplayMember = "NombreUsuario";
+}
+
+private void Button1_Click(object sender, EventArgs e)
+{
+    //correccion hacemos una validacion que no dejemos campos vacios
+if (txtOldPassword.Text.Equals(comboBox1.SelectedValue.ToString()) && txtNewPassword.Text.Length>0)
+{       
+var obtenerUsuario = (Usuario)comboBox1.SelectedItem;
+
+ActualizarControlers();
+                
+ControladorUsuario.ActualizarContrasena(obtenerUsuario.IdUsuario,
+txtNewPassword.Text);
+}
+else
+// correcion: cambiamos el mensaje ya que puede que no haya agregado nada
+    MessageBox.Show("error verifique que haya escrito algo o que su contrasena sea correcta");
+        }
+    }
+}
